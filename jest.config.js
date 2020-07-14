e6ee4f5de405b296fe4cf970948614a900b5fbef
Nattaaek Wattanuyan <w.nattaaek@yahoo.com>
2020-07-14 22:02:27 +0700

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv:['<rootDir>/src/__test__/config/importJestDOM.ts']
+  setupFilesAfterEnv:['<rootDir>/src/__test__/config/importJestDOM.ts'],
+  coverageThreshold: {
+    "global": {
+      "branches": 80,
+      "functions": 80,
+      "lines": 80,
+      "statements": 80
+    }
+  },
 };
